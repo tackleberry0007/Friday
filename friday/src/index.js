22263e8a75e8b337539501f85154d65374496850
tackleberry0007 <pankajprasad4545@gmail.com>
2023-06-06 10:00:16 +0530

    1

--- friday/src/index.js
+++ friday/src/index.js
@@ -4,12 +4,14 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import Home from './component/01Home'
+import About from './component/02About'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     {/* <App /> */}
     <Home />
+    <About />
   </React.StrictMode>
 );
 
